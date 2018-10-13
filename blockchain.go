6ef8730307564e650d420b6e623b3a6b8a12c2f0
qinglin <316032931@qq.com>
2018-10-13 13:16:45 +0800

    network-oneNodeOnly-terminalSimulate

--- blockchain.go
+++ blockchain.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"strconv"
+	"sync"
+
 	//  "fmt"
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"time"
@@ -147,17 +153,82 @@ func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload i
   w.Write(response)
 }
 
+var bcServer chan []Block
+var mutex = &sync.Mutex{}
+
+func handleConn(conn net.Conn) {
+	io.WriteString(conn, "Enter a new BPM:")
+	scanner := bufio.NewScanner(conn)
+
+	go func(){
+		for scanner.Scan() {
+			bpm, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				log.Printf("%v not a number: %v", scanner.Text(), err)
+				continue
+			}
+			newBlock, err := generateBlock(Blockchain[len(Blockchain) - 1], bpm)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if isBlockValid(newBlock, Blockchain[len(Blockchain) - 1]) {
+				newBlockchain := append(Blockchain, newBlock)
+				replaceChain(newBlockchain)
+			}
+			spew.Dump(Blockchain)
+			bcServer <- Blockchain
+			io.WriteString(conn, "\nEnter a new BPM:")
+		}
+	}()
+
+	go func(){
+		for {
+			time.Sleep(30 * time.Second)
+			mutex.Lock()
+			output, err := json.Marshal(Blockchain)
+			if err != nil {
+				log.Fatal(err)
+			}
+			mutex.Unlock()
+			io.WriteString(conn, string(output))
+		}
+	}()
+
+	for _ = range bcServer {
+		//spew.Dump(Blockchain)
+	}
+}
+
 func main() {
   err := godotenv.Load()
   if err != nil {
     log.Fatal(err)
   }
+
+  bcServer = make(chan []Block)
+
   go func() {
     t := time.Now()
     genesisBlock := Block{0, t.String(), 0, "", ""}
     spew.Dump(genesisBlock)
     Blockchain = append(Blockchain, genesisBlock)
   }()
+
+  server, err := net.Listen("tcp", ":" + os.Getenv("ADDR"))
+  if err != nil {
+  	log.Fatal(err)
+  }
+  defer server.Close()
+
+  for {
+  	conn, err := server.Accept()
+  	if err != nil {
+  		log.Fatal(err)
+	}
+  	go handleConn(conn)
+  }
+
   log.Fatal(run())
 
 }
